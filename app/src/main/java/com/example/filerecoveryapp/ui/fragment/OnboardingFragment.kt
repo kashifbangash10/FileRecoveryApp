483b898d1317a9e619f359871f0dea34ecc6ff4a
kashifbangash <kashifbangash033537@gmail.com>
2025-01-29 18:10:49 +0500

    intial commit

--- app/src/main/java/com/example/filerecoveryapp/ui/fragment/OnboardingFragment.kt
+++ app/src/main/java/com/example/filerecoveryapp/ui/fragment/OnboardingFragment.kt
@@ -1,19 +1,27 @@
 package com.example.filerecoveryapp.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.filerecoveryapp.R
 import com.example.filerecoveryapp.databinding.FragmentOnboardingBinding
 import com.example.filerecoveryapp.ui.OnbordigAdapter.OnboardingAdapter
+import com.example.filerecoveryapp.ui.activity.HomeActivity
 import com.google.android.material.tabs.TabLayoutMediator
-
 
 class OnboardingFragment : Fragment() {
     private var _binding: FragmentOnboardingBinding? = null
     private val binding get() = _binding!!
+    private lateinit var adapter: OnboardingAdapter
+    private val layouts = listOf(
+        R.layout.itemintroone,
+        R.layout.itemintrotwo,
+        R.layout.itemthree
+    )
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,16 +34,24 @@ class OnboardingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val layouts = listOf(
-            R.layout.itemintroone,
-            R.layout.itemintrotwo,
-            R.layout.itemthree
-        )
-
-        val adapter = OnboardingAdapter(layouts, requireContext())
+        adapter = OnboardingAdapter(layouts, requireContext())
         binding.viewPager.adapter = adapter
 
-        TabLayoutMediator(binding.tablayout, binding.viewPager) { _, _ -> }.attach()
+        binding.btnnext.setOnClickListener {
+            val currentItem = binding.viewPager.currentItem
+            if (currentItem < layouts.size - 1) {
+                binding.viewPager.currentItem = currentItem + 1
+            } else {
+                startActivity(Intent(requireContext(),HomeActivity::class.java))
+                requireActivity().finish()
+            }
+        }
+
+        binding.tvSkip.setOnClickListener {
+            startActivity(Intent(requireContext(),HomeActivity::class.java))
+            requireActivity().finish()
+
+        }
     }
 
     override fun onDestroyView() {
