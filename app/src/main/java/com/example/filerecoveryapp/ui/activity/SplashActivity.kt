b827727461a3cf7636874923ac62d1e163f987f2
kashifbangash <kashifbangash033537@gmail.com>
2025-01-29 10:26:54 +0500

    intial commit

---
+++ app/src/main/java/com/example/filerecoveryapp/ui/activity/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.filerecoveryapp.ui.activity
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+import com.example.filerecoveryapp.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize view binding
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }}
+
