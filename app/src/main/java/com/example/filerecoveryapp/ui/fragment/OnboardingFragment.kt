1a0f308f503638be8d011723dd0c336deaa3bcd3
kashifbangash <kashifbangash033537@gmail.com>
2025-01-29 14:50:34 +0500

    intial commit

--- app/src/main/java/com/example/filerecoveryapp/ui/fragment/OnboardingFragment.kt
+++ app/src/main/java/com/example/filerecoveryapp/ui/fragment/OnboardingFragment.kt
@@ -6,21 +6,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.filerecoveryapp.R
+import com.example.filerecoveryapp.databinding.FragmentOnboardingBinding
+import com.example.filerecoveryapp.ui.OnbordigAdapter.OnboardingAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+
 
 class OnboardingFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
+    private var _binding: FragmentOnboardingBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_onboarding, container, false)
+    ): View {
+        _binding = FragmentOnboardingBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val layouts = listOf(
+            R.layout.itemintroone,
+            R.layout.itemintrotwo,
+            R.layout.itemthree
+        )
+
+        val adapter = OnboardingAdapter(layouts, requireContext())
+        binding.viewPager.adapter = adapter
+
+        TabLayoutMediator(binding.tablayout, binding.viewPager) { _, _ -> }.attach()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
